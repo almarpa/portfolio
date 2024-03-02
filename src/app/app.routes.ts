45a038e54aba70bc38b77b5e12c7626b33847122
amarcopa <amarcopa@mercadona.es>
2024-03-02 21:09:10 +0100

    chore(app): refactor code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
