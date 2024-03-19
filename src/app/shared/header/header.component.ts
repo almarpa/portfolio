5753dff9f7ac75a7509d41699fc59e2187cb566e
amarcopa <amarcopa@mercadona.es>
2024-03-19 10:04:50 +0100

    feat(header): refactor component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,25 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxTypedJsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.component.html',
   styleUrls: ['header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   @Input() backgroundImage!: string;
-  @Input() heading!: string;
-  @Input() subHeading!: string;
+  @Input() greeting: string = '¡Hi!';
+  @Input() heading: string = "I'm Alejandro Marco Palomares";
+  @Input() subHeading: string[] = [
+    'Android Developer',
+    'FrontEnd Developer',
+    'Software Engineer',
+  ];
 
   safeBackgroudImage!: SafeStyle;
 
